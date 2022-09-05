1aca046e3fdda5be9cae68f6b71a47ba31345765
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-09-05 13:40:44 -0500

    try sapphire current tab

--- nvim/lua/colors/catppuccin.lua
+++ nvim/lua/colors/catppuccin.lua
@@ -4,7 +4,7 @@ vim.cmd("colorscheme catppuccin")
 
 -- tabs
 vim.cmd("hi TabLineSel guifg=#CDD6F4 guibg=#1E1E2E")
-vim.cmd("hi TabLineSeparatorSel guifg=#CBA6F7")
+vim.cmd("hi TabLineSeparatorSel guifg=#74c7ec")
 vim.cmd("hi TabLine guifg=#585B70 guibg=#181825")
 vim.cmd("hi TabLineFill guibg=#181825")
 
