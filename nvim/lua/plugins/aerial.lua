fa3ed7f743412c29a8afbf65dafc7afdfe885e1e
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-09-18 10:08:27 -0500

    delete todos

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -6,19 +6,6 @@ return {
 			highlight_on_jump = false,
 			max_width = 0.95,
 			min_width = 60,
-
-			-- -- would love to override these but doesn't seem possible currently
-			-- default_bindings = false,
-			-- on_attach = function(bufnr)
-			-- 	local mappings = {
-			-- 		{ "k", ":AerialPrev<CR>" },
-			-- 		{ "j", ":AerialNext<CR>" },
-			-- 	}
-			--
-			-- 	for _, m in ipairs(mappings) do
-			-- 		vim.api.nvim_buf_set_keymap(bufnr, "n", m[1], m[2], { noremap = true, silent = true })
-			-- 	end
-			-- end,
 		})
 	end,
 }
