61a091e4e635c3bc4c3ae90548215bcd493f8b06
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-09-26 08:35:05 -0500

    mappings for diffview

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -11,12 +11,14 @@ return {
 				file_panel = {
 					["<c-u>"] = actions.scroll_view(-0.25),
 					["<c-d>"] = actions.scroll_view(0.25),
+					["o"] = actions.goto_file_tab,
 					["q"] = ":DiffviewClose<CR>",
 				},
 
 				file_history_panel = {
 					["<c-u>"] = actions.scroll_view(-0.25),
 					["<c-d>"] = actions.scroll_view(0.25),
+					["o"] = actions.open_in_diffview,
 					["q"] = ":DiffviewClose<CR>",
 				},
 			},
