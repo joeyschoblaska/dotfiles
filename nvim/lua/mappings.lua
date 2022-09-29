94b87c0da270e105c432c9416ef007bbf55fb756
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-09-29 13:25:40 -0500

    shortcut for grepping selection in open files

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -35,6 +35,7 @@ local mappings = {
 	{ "n", "<leader>ff", ":Telescope file_browser path=%:p:h select_buffer=true<CR>" }, -- open current file in browser
 	{ "n", "<leader>fh", ":cd " .. vim.fn.getcwd() .. "<CR>" }, -- reset cwd to its value when vim session began
 	{ "n", "<leader>ba", ":Telescope live_grep grep_open_files=true<CR>" }, -- live grep in all open buffers
+	{ "v", "<leader>ba", [["9y<ESC>:Telescope live_grep grep_open_files=true default_text=<C-r>9<CR>]] }, -- yank selection into reg 9 and grep open files for it
 
 	-- notes
 	{ "n", "<leader>nt", ":Telescope find_files hidden=false cwd=$NOTES_DIR<CR>" }, -- open file in notes dir
