10789f6ea028d53322d948a3be405608f05ee1cf
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-10-07 09:33:43 -0500

    less flashy indent-blankline

--- nvim/lua/plugins/indent_blankline.lua
+++ nvim/lua/plugins/indent_blankline.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		require("indent_blankline").setup({
 			show_trailing_blankline_indent = false,
-			show_current_context = true,
+			show_current_context = false,
 			char_highlight_list = {
 				"IndentBlanklineIndent1",
 				"IndentBlanklineIndent2",
