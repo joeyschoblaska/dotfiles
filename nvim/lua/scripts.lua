24c68be5867a4260460d0705882d603959387bd7
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-10-11 13:08:06 -0500

    this is set globally

--- nvim/lua/scripts.lua
+++ nvim/lua/scripts.lua
@@ -1,8 +1,8 @@
 M = {}
 
 M.toggle_diagnostic_virtual_text = function()
-	local new_val = not vim.b.diagnosticVirtualText
-	vim.b.diagnosticVirtualText = new_val
+	local new_val = not vim.g.diagnosticVirtualText
+	vim.g.diagnosticVirtualText = new_val
 	vim.diagnostic.config({
 		virtual_text = new_val,
 	})
