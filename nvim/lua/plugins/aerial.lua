e418897ac1ffe8989e79ca5f2892b9e66627bdec
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-10-13 09:42:06 -0500

    close aerial when switching buffers

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -7,6 +7,7 @@ return {
 	config = function()
 		require("aerial").setup({
 			close_on_select = true,
+			close_automatic_events = { "unfocus", "switch_buffer" },
 			highlight_on_jump = false,
 			max_width = 0.95,
 			min_width = 60,
