db34716bd3adb0d2f747b95a43f8e1db92522656
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-12-22 16:27:29 -0600

    nvim-tree wip

--- nvim/lua/plugins/nvim_tree.lua
+++ nvim/lua/plugins/nvim_tree.lua
@@ -1,6 +1,29 @@
 return {
 	"nvim-tree/nvim-tree.lua",
 	config = function()
-		require("nvim-tree").setup()
+		require("nvim-tree").setup({
+			view = {
+				width = 35,
+				float = {
+					enable = true,
+					-- todo: make this dynamic https://pastebin.com/5Hi876Md
+					open_win_config = {
+						width = 50,
+					},
+				},
+				mappings = {
+					list = {
+						{ key = "s", action = "" },
+					},
+				},
+			},
+			tab = {
+				sync = {
+					open = true,
+					close = true,
+					ignore = {},
+				},
+			},
+		})
 	end,
 }
